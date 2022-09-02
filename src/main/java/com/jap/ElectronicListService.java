bcfbab45a8765330207f6fdf032af409ee84d2a0
rishavemail786 <rishavemail786@gmail.com>
2022-09-02 09:00:33 +0530

    SOLUTION DONE

--- src/main/java/com/jap/ElectronicListService.java
+++ src/main/java/com/jap/ElectronicListService.java
@@ -7,22 +7,38 @@ import java.util.List;
 public class ElectronicListService {
     //add all the electronics items inside the List
     public static List<String> addElectronicsItemsToList(String electronicsItems) {
-       List<String> itemList = null;
+       List<String> itemList = new ArrayList<>();
+       String[] value=electronicsItems.split(",");
+       itemList.add("computer");
+       itemList.add("Refrigerator");
+       itemList.add("smartphone");
+       itemList.add("printer");
+
+        for (String element: itemList) {
+            }
         return itemList;
     }
 
     //search the electronic item from the list and return the index position
     public static int searchElectronicItemInList(List<String> itemList, String searchItem) {
-        return -1;
+        return itemList.indexOf(searchItem);
     }
 
     //remove the electronic item from the list using iterator
     public static boolean removeElectronicsItemFromList(List<String> itemList, String removeItem)
+
     {
         boolean flag = false;
+        Iterator<String> iterator = itemList.iterator();
+        while (iterator.hasNext()){
+            if (iterator.next().equals(removeItem)){
+                iterator.remove();
+                flag=true;
+            }
+        }
 
         return flag;
     }
 
-
 }
+
